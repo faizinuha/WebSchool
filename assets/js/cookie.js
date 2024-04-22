bafba26b5038d3230093677d8789a390c468cb87
MarDev <rozakadm@gmail.com>
2024-04-22 19:49:53 +0700

    changerelative;relative;relative;

---
+++ assets/js/cookie.js
@@ -0,0 +1,18 @@
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => {
+  cookieBox.classList.add("show");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+
+      if (button.id == "acceptBtn") {
+        document.cookie = "cookieBy=codinglab; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+window.addEventListener("load", executeCodes);
